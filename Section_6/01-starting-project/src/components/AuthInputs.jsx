f5159ce8d5ccd3297eb5116a61b90c763cdfc972
Sugeun Song <songsurl@naver.com>
2025-03-31 15:56:57 +0900

    CSS 파일과 CSS 클래스를 사용한 동적 및 조건적 스타일링

--- Section_6/01-starting-project/src/components/AuthInputs.jsx
+++ Section_6/01-starting-project/src/components/AuthInputs.jsx
@@ -24,13 +24,13 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <div className="controls">
         <p>
-          <label>Email</label>
+          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
           <input
             type="email"
-            style={{ 
-              backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db;'
-            }}
-            // className={emailNotValid ? 'invalid' : undefined}
+            // style={{ 
+            //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
+            // }}
+            className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
