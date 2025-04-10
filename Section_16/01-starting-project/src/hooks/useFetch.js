12524239d7e767e47ef5558a7d26ba1ffa16b269
Sugeun Song <songsurl@naver.com>
2025-04-10 15:22:28 +0900

    섹션 16: 커스텀 리액트 Hook 빌드

---
+++ Section_16/01-starting-project/src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+export const useFetch = (fetchFunction, initialValue) => {
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState(initialValue);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFunction();
+        setFetchedData(data);
+      } catch (error) {
+        setError({ message: error.message || 'Failed to fetch data.' });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFunction]);
+
+  return {
+    isFetching,
+    error,
+    fetchedData,
+    setFetchedData
+  };
+};
